7797f8d73f87404df0ae74fb760198ecfd42fa43
Simon Holt <simonholt83@gmail.com>
2013-09-10 06:16:49 -0500

    Cleaned up some javascript

--- js/ding-navigation-box.js
+++ js/ding-navigation-box.js
@@ -1,80 +1,85 @@
 (function($) {
   
-  Drupal.behaviors.dingNavigationBox = {
+  // Declare variables
+  var active;
+  // Store our module-name to use as prefix in selectors.
+  var prefix = ".ding-navigation-box-";
+  var contentWidth;
+  var contentBorderLeft;
+  var borderShadow;
+  
+  Drupal.behaviors.dingNavigationBox = {   
     
     attach: function(context, settings) {
       
-      // Store our module-name to use as prefix in selectors.
-      var prefix = ".ding-navigation-box-";
       // Store the width of the content area container.
-      var contentWidth = $(prefix + "content-areas").width();
+      contentWidth = $(prefix + "content-areas").width();
       // Store the left border of the content area container.
-      var contentBorderLeft = $(prefix + "content-areas").css("border-left");
+      contentBorderLeft = $(prefix + "content-areas").css("border-left");
       // Store the border-shadow of the content area container,
-      var borderShadow = $(prefix + "content-areas").css("box-shadow");
+      borderShadow = $(prefix + "content-areas").css("box-shadow");
       
-      // We initialize the navigaion box as inactive, setting active false
-      // and hiding the content area.
-      var active = false;
-      hideContentArea();    
-
       // The content should have a min-height equal to the height of the items
       // Since the height of the items varies with number of items and the 
       // individual height of items, we calculate the actual height of the 
       // items here and set that as the min-height for the content.
-      setUpContentMinHeight();    
-        
+      setContentMinHeight();    
+      
+      // Since the navigation box should start out as inactive, we run the 
+      // deactivation function once when this attach function is first called.
+      $(".ding-navigation-box").once("ding-navigation-box-attach", function() {
+        deactivateNavigationBox();
+        // Also run the deactivation function every the mouse leaves the 
+        // navigation box.
+        $(".ding-navigation-box").mouseleave(deactivateNavigationBox);  
+      });
+      
       // Iterate over each navigation item and attach an eventn hanlder for 
       // the mouseenter event.
       $(prefix + "item").each(function(index) {
         // Ensure each handler only gets attached once.
         $(this, context).once("ding-navigation-item-attach", function() {
           $(this).mouseenter(function(){
-            // If the navigation box was not active we start the animation.
-            if (!active) {
-              active = true;
-              $(prefix + "content-areas")
-                .css("border-left", contentBorderLeft)
-                .css("box-shadow", borderShadow)
-                .animate({
-                  width: contentWidth
-                }, 500);
-            }
-            // Hide any active content area.
+            // If the navigation box was not active we activate it before we 
+            // show the associated content.
+            if (!active) {activateNavigationBox();}
+            // Hide any active content areas.
             $(prefix + "content-area").hide();
             // Show the content area associated with this item.
-            $(prefix + "content-areas > div:nth-of-type(" + (index + 1) + ")").show();
+            $(prefix  + "content-areas > div:nth-of-type(" + (index + 1) + ")").show();
           });
         });
       });
       
-      // Attach an event hanlder for the mouseleave event to the navigation box.
-      $(".ding-navigation-box").once("ding-navigation-box-attach", function() {
-        $(".ding-navigation-box").mouseleave(function() {
-          hideContentArea();
-          active = false;
-        });  
-      });
-      
-      function setUpContentMinHeight() {
-        var itemsHeight = $(prefix + "items").height();
-        var borderHeight = $(prefix + "content-areas").css("border-bottom-width");
-        var minHeight = itemsHeight - parseInt(borderHeight);
-        $(prefix + "content-areas").css("min-height", minHeight);
-      }
-      
-      function hideContentArea() {
-        $(prefix + "content-area").hide();
-        $(prefix + "content-areas")
-          .stop()
-          .width("0")
-          .css("border-left", "none")
-          .css("box-shadow", "none");
-      }
-      
     }
-    
   };
+  
+  function activateNavigationBox() {
+    active = true;
+    $(prefix + "content-areas")
+      .css("border-left", contentBorderLeft)
+      .css("box-shadow", borderShadow)
+      .animate({
+        width: contentWidth
+      }, 500);    
+  }
+  
+  function deactivateNavigationBox() {
+    active = false;
+    $(prefix + "content-area").hide();
+    $(prefix + "content-areas")
+      .stop()
+      .width("0")
+      .css("border-left", "none")
+      .css("box-shadow", "none");    
+  }
+  
+  function setContentMinHeight() {
+    var itemsHeight = $(prefix + "items").height();
+    var borderHeight = $(prefix + "content-areas").css("border-bottom-width");
+    var minHeight = itemsHeight - parseInt(borderHeight);
+    $(prefix + "content-areas").css("min-height", minHeight);
+  }
   
 })(jQuery)
 
