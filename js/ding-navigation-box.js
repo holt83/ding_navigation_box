0004f0ee0b9de633777cff8fcafab17c2cfae59f
holt83 <simonholt83@gmail.com>
2013-11-07 04:59:34 -0800

    JS Changes

--- js/ding-navigation-box.js
+++ js/ding-navigation-box.js
@@ -7,37 +7,42 @@
       $(".ding-nav-box-content-area").hide();
       $(".ding-nav-box-activation-arrow").hide();
       // Activate the default navigation item.
-      activateNavigationItem(settings.dingNavigationBox.defaultItemNumber);
-      // Iterate over each activation area.
+      toggleNavigationItem(settings.dingNavigationBox.defaultItemNumber, true);
+      // Iterate over each activation area and attach event-handlers.
       $(".ding-nav-box-activation-area", context).each(function(index) {
     	var itemNumber = index + 1;  
     	$(this).click(function() {
-    	  toggleNavigationItem(activeItemNumber, false);	
+    	  // Deactivate the active navigation item.	
+    	  toggleNavigationItem(activeItemNumber, false);
+    	  // Activate this naviation item.
     	  toggleNavigationItem(itemNumber, true);
     	});
     	$(this).hover(function() {
-    	  $(this).css("background-color", "black");	    		  
-    	},function() {
+    	  // Only add the hover class if the item is NOT the active item.	
     	  if (itemNumber != activeItemNumber) {
-    		$(this).css("background-color", "transparent");	    		  
-    	  }	
+    		$(this).addClass(settings.dingNavigationBox.hoverClass);	    		  
+    	  }
+    	},function() {
+    	  $(this).removeClass(settings.dingNavigationBox.hoverClass);	    		  
     	});
       });
     }
   };
   
+  // Deactivates or activates the given navigation item.
   function toggleNavigationItem(itemNumber, activate) {
 	var contentArea = $(".ding-nav-box-content-areas > div:nth-of-type(" + itemNumber + ")");
 	var activationAreaSelector = ".ding-nav-box-activation-areas-center-wrapper > div:nth-of-type(" + itemNumber + ")";
 	if (activate) {
 	  contentArea.show();
-	  $(activationAreaSelector).css("background-color", "black");
+	  $(activationAreaSelector).addClass(Drupal.settings.dingNavigationBox.activationClass);
+	  $(activationAreaSelector).removeClass(Drupal.settings.dingNavigationBox.hoverClass);
 	  $(activationAreaSelector + " > .ding-nav-box-activation-arrow").show();
 	  activeItemNumber = itemNumber;
 	}
 	else {
 	  contentArea.hide();
-	  $(activationAreaSelector).css("background-color", "transparent");
+	  $(activationAreaSelector).removeClass(Drupal.settings.dingNavigationBox.activationClass);
 	  $(activationAreaSelector + " > .ding-nav-box-activation-arrow").hide();
 	  activeItemNumber = 0;
 	}
