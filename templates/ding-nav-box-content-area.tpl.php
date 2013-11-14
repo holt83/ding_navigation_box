180cb58805eaaedf5a4b1f32ec0ec3ee46fb0658
holt83 <simonholt83@gmail.com>
2013-11-14 06:39:13 -0800

    Change to relative units

--- templates/ding-nav-box-content-area.tpl.php
+++ templates/ding-nav-box-content-area.tpl.php
@@ -22,5 +22,10 @@
 
 <div class="<?php print $classes; ?>" <?php print $attributes ?>>
   <h3><?php print $item_position; ?></h3>
+  <h3>This is a test line inserted to test max-width property on the ding navigation box</h3>
+  <br>
+  <hr>
+  <br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   <?php// print render($entry_content); ?> 
 </div>
