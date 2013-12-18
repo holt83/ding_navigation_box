20f1d6483d84487574683e418fcee3597762dee9
holt83 <simonholt83@gmail.com>
2013-12-18 14:35:07 -0800

    Changes to admin ui

--- js/ding-navigation-box-admin.js
+++ js/ding-navigation-box-admin.js
@@ -2,13 +2,18 @@
 	Drupal.behaviors.dingNavigationBoxAdmin = {
 		attach: function(context, settings) {
 			var prefix = "#ding-navigation-box-admin";
+			/*
 			$(prefix + " .edit-item-links a").hide();
 			var activeItem = settings.dingNavigationBox.activeItemPosition;
-			$(prefix + " .edit-item-links a:nth-of-type(" + activeItem + ")").show();
+			$(prefix + " .edit-item-links a:nth-of-type(" + activeItem + ")").show();*/
+
+
 			$(".ding-navigation-box .activation-area", context).each(function(index) {
 				$(this).click(function() {
-					$(prefix + " .edit-item-links a").hide();
-					$(prefix + " .edit-item-links a:nth-of-type(" + (index + 1) + ")").show();					
+					var dniid = $(this).data("dniid");
+					var title = $(".ding-navigation-box a:nth-of-type(" + (index + 1) + ") .full").text();
+					$("#ding-navigation-box-admin .edit-item-link a").text("Edit " + title);
+					//$("#ding-navigation-box-admin .edit-item-link a").attr("href", dniid);
 				})
 			});	
 		}
