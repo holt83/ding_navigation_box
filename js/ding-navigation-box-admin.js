9b1952905cb33a38765f250a411fd81737475f1f
holt83 <simonholt83@gmail.com>
2013-12-19 14:12:58 -0800

    Added data attr for edit link in admin page

--- js/ding-navigation-box-admin.js
+++ js/ding-navigation-box-admin.js
@@ -1,19 +1,13 @@
 (function($) {
 	Drupal.behaviors.dingNavigationBoxAdmin = {
 		attach: function(context, settings) {
-			var prefix = "#ding-navigation-box-admin";
-			/*
-			$(prefix + " .edit-item-links a").hide();
-			var activeItem = settings.dingNavigationBox.activeItemPosition;
-			$(prefix + " .edit-item-links a:nth-of-type(" + activeItem + ")").show();*/
-
-
+			var adminURI = settings.dingNavigationBoxAdmin.adminURI;
 			$(".ding-navigation-box .activation-area", context).each(function(index) {
 				$(this).click(function() {
 					var dniid = $(this).data("dniid");
 					var title = $(".ding-navigation-box a:nth-of-type(" + (index + 1) + ") .full").text();
 					$("#ding-navigation-box-admin .edit-item-link a").text("Edit " + title);
-					//$("#ding-navigation-box-admin .edit-item-link a").attr("href", dniid);
+					$("#ding-navigation-box-admin .edit-item-link a").attr("href", "/" + adminURI + "/edit/" + dniid);
 				})
 			});	
 		}
