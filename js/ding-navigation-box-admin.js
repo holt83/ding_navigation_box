bd96b5ab4752d9c46f44f95eab93414814a8ce32
holt83 <simonholt83@gmail.com>
2014-01-19 06:52:58 -0800

    Added transaction to change position

--- js/ding-navigation-box-admin.js
+++ js/ding-navigation-box-admin.js
@@ -1,18 +1,22 @@
 (function($) {
-	Drupal.behaviors.dingNavigationBoxAdmin = {
+
+	Drupal.behaviors.dingNavigationBoxAdmin = {		
 		attach: function(context, settings) {
-			// Updates the edit link when a new navigation item is activated.
+
+			// Edit navigation item.
 			var adminPath = settings.dingNavigationBox.adminPath
 			$(".ding-navigation-box .activation-area", context).each(function(index) {
 				$(this).click(function(e) {
 					e.preventDefault;
 					var dniid = $(this).data("dniid");
 					var title = $(this).find(".full").text();
-					$("#edit-item-link").text("Edit " + title).attr("href", "/" + adminPath + "/edit/" + dniid);
+					$("#edit-item-link")
+						.text("Edit " + title)
+						.attr("href", "/" + adminPath + "/edit/" + dniid);
 				})
 			});
 
-			// Slideshow demo
+			// Slideshow
 			var slideshowDemo = $("#slideshow-demo");
 			var slideshowDemoRunning = false;
 			slideshowDemo.click(function(e) {
@@ -29,63 +33,56 @@
 				slideshowDemoRunning = !slideshowDemoRunning;			
 			});
 
-			// Attach event handlers to button that change position of navigation items.
+			// Change position.
 			$("a.move-item-button").click(function(e) {
 				e.preventDefault();
+				// Display info.
+				var info = $("#change-position-info")
+				  .removeClass("success")
+				  .removeClass("error")
+				  .text(Drupal.t("Changing position..."));
+				// Disable move buttons while performing request.
+				var moveButtons = $(".move-item-button").addClass("disabled");				  
+				// Get the area to change position on.
+				var area = $(".ding-navigation-box .activation-area.active-item");
+				var action = ($(this).attr("id") == "move-item-down" ? "down" : "up");
+				var itemID = area.data("dniid");				
+				var jsonData = {"itemID": itemID, "action": action};
+				// Get the other area affected by the change.
+				var other = action == "down" ? area.next() : area.prev();
 
-				// Display message to user telling that the ajax request is being sent.
-				var message = Drupal.t("Changing position..."); 
-				$("#change-position-info").removeClass("success").removeClass("error").text(message);
-
-				var activeActivationArea = $(".ding-navigation-box .activation-area.active-item");
-				changePositionAction = "up";
-				otherActivationArea = activeActivationArea.prev();
-				if ($(this).attr("id") == "move-item-down") {
-					changePositionAction = "down";
-					otherActivationArea = activeActivationArea.next();					
+				// Only perform request if the position change is valid.
+				if (other.length > 0) {
+					$.ajax({
+						type: "POST",
+						url: "/" + adminPath + "/change-item-position",
+						dataType: "json",
+						data: jsonData,
+						success: success,
+					});				
+				}
+				else {
+					info
+						.addClass("error")
+						.text(Drupal.t("Can't move further " + action));
+					moveButtons.removeClass("disabled");					
 				}
 
-				// Out of bounds - cancel AJAX request.
-				if (otherActivationArea.length == 0) {
-					message = Drupal.t("That item can't move further " + changePositionAction);
-					$("#change-position-info").addClass("error").text(message);
-					return false;					
-				}
-
-				// Disable move links while performing AJAX request.
-				$("a.move-item-button").addClass("disabled");
-				// Get ID of the item to change position on.
-				var activeItemID = activeActivationArea.data("dniid");
-				// Make the ajax call to the callback function responsible for changing
-				// position of navigation items.
-				$.ajax({
-					type: "POST",
-					url: "/" + adminPath + "/change-item-position",
-					dataType: "json",
-					data: {"activeItemID": activeItemID, "changePositionAction": changePositionAction},
-					success: function(data) {
-						// Enable move buttons again.
-						$("a.move-item-button").removeClass("disabled");
-						// If success, update the position on the client.
-						if (data === "success") {						
-							if (changePositionAction  == "up") {
-								otherActivationArea.before(activeActivationArea);
-							}
-							else { 
-								otherActivationArea.after(activeActivationArea);
-							}
-							message = Drupal.t("Position changed succesfully");
-							$("#change-position-info").addClass("success");
-						}
-						// If error, notify the user.
-						else if (data === "error") {
-							message = Drupal.t("There was a problem changing positions");
-							$("#change-position-info").addClass("error");
-						}
-
-						$("#change-position-info").text(message);
+				function success(data) {
+					if (data === "success") {						
+						if (action  == "up") other.before(area);
+						else other.after(area);
+						info
+							.addClass("success")
+							.text(Drupal.t("Position changed succesfully"));
 					}
-				});
+					else if (data == "error") {
+						info
+							.addClass("error")
+							.text(Drupal.t("There was an error changin position"));
+					}
+					moveButtons.removeClass("disabled");					
+				}
 			});
 		}
 	}
